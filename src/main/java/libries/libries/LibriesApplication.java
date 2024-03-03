df6b64dffbb48faaebc3d861377115a2a9da2400
2desoo <marcus60.2001@gmail.com>
2024-03-03 19:02:19 +0300

    Initial commit

---
+++ src/main/java/libries/libries/LibriesApplication.java
@@ -0,0 +1,13 @@
+package libries.libries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibriesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/libries/libries/LibriesApplicationTests.java
@@ -0,0 +1,13 @@
+package libries.libries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
